350fbdf35465afe46515c522d91f02baa79f7525
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-09-12 16:43:47 +0100

    Uploaded SQL project folder

---
+++ SQLing/SQLing/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SQLing
+{
+    partial class frmPeopleDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtPeople = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtName.Location = new System.Drawing.Point(60, 89);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(259, 32);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblName.Location = new System.Drawing.Point(60, 45);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(194, 26);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "What is your name";
+            // 
+            // txtAge
+            // 
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtAge.Location = new System.Drawing.Point(60, 175);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(70, 32);
+            this.txtAge.TabIndex = 0;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAge.Location = new System.Drawing.Point(60, 146);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(51, 26);
+            this.lblAge.TabIndex = 1;
+            this.lblAge.Text = "Age";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnAdd.Location = new System.Drawing.Point(215, 161);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 57);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnRead.Location = new System.Drawing.Point(215, 248);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(120, 57);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtPeople
+            // 
+            this.txtPeople.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPeople.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtPeople.Location = new System.Drawing.Point(408, 89);
+            this.txtPeople.Multiline = true;
+            this.txtPeople.Name = "txtPeople";
+            this.txtPeople.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPeople.Size = new System.Drawing.Size(403, 255);
+            this.txtPeople.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnDelete.Location = new System.Drawing.Point(215, 328);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 57);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtID.Location = new System.Drawing.Point(60, 262);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(70, 32);
+            this.txtID.TabIndex = 0;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblID.Location = new System.Drawing.Point(60, 233);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(34, 26);
+            this.lblID.TabIndex = 1;
+            this.lblID.Text = "ID";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnUpdate.Location = new System.Drawing.Point(215, 403);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(120, 57);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // frmPeopleDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 485);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPeople);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmPeopleDatabase";
+            this.Text = "People DataBase Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtPeople;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ SQLing/SQLing/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SQLing
+{
+    public partial class frmPeopleDatabase : Form
+    {
+        public frmPeopleDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Go to properties of db and copy and paste connectionString value
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\m2404024\\OneDrive - Middlesbrough College\\YR2\\Trevor\\Programing\\SQLing\\SQLing\\PeopleDB.mdf\";Integrated Security=True;Connect Timeout=30";
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //Use stored procedure
+
+            SqlCommand command = new SqlCommand("CreateNewPersonRecord", sqlConnection);
+
+
+            command.CommandType = CommandType.StoredProcedure;
+
+
+            //Input name and age from form
+
+            string name = txtName.Text;
+
+            int age = int.Parse(txtAge.Text);
+
+
+            //Call stored procedure passing name and age as parameters
+
+            command.Parameters.AddWithValue("@Name", name);
+
+            command.Parameters.AddWithValue("@Age", age);
+
+
+
+            //Open connection to database, execute stored procedure and close the connection
+
+            sqlConnection.Open();
+
+
+
+            command.ExecuteNonQuery();
+
+
+            sqlConnection.Close();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //Read a person record
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\m2404024\\OneDrive - Middlesbrough College\\YR2\\Trevor\\Programing\\SQLing\\SQLing\\PeopleDB.mdf\";Integrated Security=True;Connect Timeout=30";
+
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+            SqlCommand cmd = new SqlCommand("GetPersonDetails", sqlConnection);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+
+            sqlConnection.Open();
+
+            sd.Fill(dt);
+
+            sqlConnection.Close();
+
+
+            //Read rows of database and extract fields
+
+            txtPeople.Text = string.Empty;
+
+            foreach (DataRow dr in dt.Rows)
+
+            {
+
+                int personId = (int)(dr["Id"]);
+
+                string personName = (string)(dr["Name"]);
+
+                int personAge = (int)(dr["Age"]);
+
+
+                txtPeople.AppendText(personId.ToString() + "\t" + personName + "\t" + personAge.ToString() + Environment.NewLine);
+
+
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Use stored procedure to delete record(s) containing a given name
+
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\m2404024\\OneDrive - Middlesbrough College\\YR2\\Trevor\\Programing\\SQLing\\SQLing\\PeopleDB.mdf\";Integrated Security=True;Connect Timeout=30";
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //USE STORED PROCEDURE
+
+            SqlCommand command = new SqlCommand("DeletePerson", sqlConnection);
+
+
+            command.CommandType = CommandType.StoredProcedure;
+
+
+            string name = txtName.Text;
+            string id = txtID.Text;
+
+            command.Parameters.AddWithValue("@Name", name);
+            command.Parameters.AddWithValue("@Id", id);
+
+
+            sqlConnection.Open();
+
+
+
+            command.ExecuteNonQuery();
+
+
+            sqlConnection.Close();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\m2404024\\OneDrive - Middlesbrough College\\YR2\\Trevor\\Programing\\SQLing\\SQLing\\PeopleDB.mdf\";Integrated Security=True;Connect Timeout=30";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //USE STORED PROCEDURE
+
+            SqlCommand command = new SqlCommand("UpdatePerson", sqlConnection);
+
+
+            command.CommandType = CommandType.StoredProcedure;
+
+
+            string name = txtName.Text;
+
+            int age = int.Parse(txtAge.Text);
+
+            string Id = txtID.Text;
+
+            command.Parameters.AddWithValue("@Id", Id);
+
+            command.Parameters.AddWithValue("@Name", name);
+
+            command.Parameters.AddWithValue("@Age", age);
+
+
+            sqlConnection.Open();
+
+
+
+            command.ExecuteNonQuery();
+
+
+            sqlConnection.Close();
+        }
+    }
+}
+
