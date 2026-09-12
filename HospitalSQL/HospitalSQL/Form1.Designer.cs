fcd3002ccd7bcf021ef789ffbc356e23fafce517
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-09-12 16:59:52 +0100

    Uploaded SQL Hospital project folder

---
+++ HospitalSQL/HospitalSQL/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace HospitalSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnAdd.Location = new System.Drawing.Point(101, 206);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(119, 42);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnDelete.Location = new System.Drawing.Point(101, 254);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 42);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnRead.Location = new System.Drawing.Point(101, 158);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(119, 42);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnUpdate.Location = new System.Drawing.Point(101, 302);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(119, 42);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtId.Location = new System.Drawing.Point(101, 114);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(119, 32);
+            this.txtId.TabIndex = 4;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblId.Location = new System.Drawing.Point(105, 85);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(108, 26);
+            this.lblId.TabIndex = 5;
+            this.lblId.Text = "Patient ID";
+            // 
+            // txtList
+            // 
+            this.txtList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtList.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtList.Location = new System.Drawing.Point(867, 101);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.ReadOnly = true;
+            this.txtList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtList.Size = new System.Drawing.Size(379, 512);
+            this.txtList.TabIndex = 6;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblHeader.Location = new System.Drawing.Point(101, 377);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(70, 37);
+            this.lblHeader.TabIndex = 5;
+            this.lblHeader.Text = "Info";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(105, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FirstName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(106, 491);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LastName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(276, 571);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DOB";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtFirstName.Location = new System.Drawing.Point(111, 456);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtFirstName.Size = new System.Drawing.Size(284, 32);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtLastName.Location = new System.Drawing.Point(111, 520);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtLastName.Size = new System.Drawing.Size(284, 32);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtDOB.Location = new System.Drawing.Point(281, 600);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtDOB.Size = new System.Drawing.Size(114, 32);
+            this.txtDOB.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(108, 571);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtAddress.Location = new System.Drawing.Point(113, 600);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtAddress.Size = new System.Drawing.Size(162, 32);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label5.Location = new System.Drawing.Point(108, 647);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 26);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtEmail.Location = new System.Drawing.Point(113, 676);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtEmail.Size = new System.Drawing.Size(284, 32);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label6.Location = new System.Drawing.Point(108, 720);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Phone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtPhone.Location = new System.Drawing.Point(113, 758);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtPhone.Size = new System.Drawing.Size(284, 32);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 798);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPhone;
+    }
+}
+
---
+++ HospitalSQL/HospitalSQL/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace HospitalSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadList()
+        {
+
+            // Connection string to refer to database
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"PATH_TO_DB\";Integrated Security=True;Connect Timeout=30";
+
+            // Establishes a connection
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            // Gets the procedure we will be using
+            // GetPatientDetail --> Returns everything in the Patients table
+            SqlCommand cmd = new SqlCommand("GetPatientDetails", sqlConnection);
+
+            // Tells what type of command we will be using
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            // Creates a table 
+            DataTable dt = new DataTable();
+
+            // Opens the connection 
+            sqlConnection.Open();
+
+            adapter.Fill(dt);
+
+            // Closes a connection important when another function wants to open a connection
+            sqlConnection.Close();
+
+            // Empty the list so the text doesn't pile ontop of old data
+            txtList.Text = string.Empty;
+
+            // go through each element in table 
+            foreach (DataRow dr in dt.Rows)
+            {
+                int id = (int)dr["ID"];
+
+                string firstName = ((string)dr["First_Name"]).Trim();
+
+                string lastName = ((string)dr["Last_Name"]).Trim();
+
+
+                txtList.AppendText($"{id.ToString()}: {firstName} {lastName}{Environment.NewLine}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+
+
+        // Reads the ID and output details
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"PATH_TO_DB\";Integrated Security=True;Connect Timeout=30";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            // FindAndGetPatient --> Searches the patient by id and return all information about them
+            SqlCommand cmd = new SqlCommand("FindAndGetPatient", sqlConnection);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            string id = txtId.Text;
+
+            // Assigns the variables with a value
+            cmd.Parameters.AddWithValue("Id", id);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            sqlConnection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            adapter.Fill(dt);
+
+            sqlConnection.Close();
+
+            // Asign information to variables
+            // As IDs are unique, no need for looping
+
+            DataRow dr = dt.Rows[0];
+
+            string firstName = ((string)dr["First_Name"]).Trim();
+            string lastName = ((string)dr["Last_Name"]).Trim();
+
+            string DOB = dr["DOB"].ToString().Split(' ')[0];
+            string address = ((string)dr["Address"]).Trim();
+
+            string tel = ((string)dr["Tel"]).Trim();
+            string email = ((string)dr["Email"]).Trim();
+
+
+            // Assign to textboxes
+
+            txtFirstName.Text = firstName;
+            txtLastName.Text = lastName;
+
+            txtDOB.Text = DOB;
+            txtAddress.Text = address;
+
+            txtPhone.Text = tel;
+            txtEmail.Text = email;
+
+        }
+
+        // Updates the ID entered
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"PATH_TO_DB\";Integrated Security=True;Connect Timeout=30";
+
+            // Get the Input from boxes
+            string id = txtId.Text;
+
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+
+            string DOB = txtDOB.Text;
+            string address = txtAddress.Text;
+
+            string tel = txtPhone.Text;
+            string email = txtEmail.Text;
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            // UpdatePaitient --> Gets paitient ID and updates the values in the DB
+            SqlCommand cmd = new SqlCommand("UpdatePaitient", sqlConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            // Assigns the variables with a value
+            cmd.Parameters.AddWithValue("Id", id);
+
+            cmd.Parameters.AddWithValue("FirstName", firstName);
+            cmd.Parameters.AddWithValue("LastName", lastName);
+
+            cmd.Parameters.AddWithValue("DOB", DOB);
+            cmd.Parameters.AddWithValue("Address", address);
+
+            cmd.Parameters.AddWithValue("Tel", tel);
+            cmd.Parameters.AddWithValue("Email", email);
+
+
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            LoadList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"PATH_TO_DB\";Integrated Security=True;Connect Timeout=30";
+
+            // Get the Input from boxes
+            string id = txtId.Text;
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //  --> Gets paitient ID and updates the values in the DB
+            SqlCommand cmd = new SqlCommand("DeletePaitient", sqlConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            cmd.Parameters.AddWithValue("Id", id);
+
+
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            LoadList();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
