fa39cef9a03c29f1a22fdb0e71aa0ce153df0358
Ibrahim Maqsood <imaqsood360@outlook.com>
2026-09-12 16:51:09 +0100

    Refactor connection string to use placeholder
    Updated connection string to use a placeholder for the database path.

--- SQLing/SQLing/Form1.cs
+++ SQLing/SQLing/Form1.cs
@@ -15,7 +15,7 @@ namespace SQLing
         private void btnAdd_Click(object sender, EventArgs e)
         {
             // Go to properties of db and copy and paste connectionString value
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\m2404024\\OneDrive - Middlesbrough College\\YR2\\Trevor\\Programing\\SQLing\\SQLing\\PeopleDB.mdf\";Integrated Security=True;Connect Timeout=30";
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"PATH_TO_DB";Integrated Security=True;Connect Timeout=30";
 
 
             SqlConnection sqlConnection = new SqlConnection(connectionString);
